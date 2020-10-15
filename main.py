67e975362058bb5a92f46bb620fd1fcb3aedd755
Alec <alecyang@uvic.ca>
2020-10-14 23:35:27 -0700

    Init

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-def hello():
-    ...
+import os
+import glob
+import pandas as pd
 
 
-if __name__ == '__main__':
-    hello()
-    ...
+def combine_csv(directory_path: str):
+    os.chdir(directory_path)
+    csv = 'csv'
+    file_names = [i for i in glob.glob(f'*.{csv}')]
+    new_csv = pd.concat([pd.read_csv(f) for f in file_names])
+    new_csv.to_csv('new_csv.csv', index=False, encoding='utf-8-sig')
+
+
+combine_csv("your path")
